8b417718e4243eba74596b2be3717bc45b6455a2
Gabriel Henriques <soadhenri@gmail.com>
2021-08-22 12:36:47 -0300

    add new input of payment to request api

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let form = document.getElementById("form");
+let formPayment = document.getElementById("formPayment");
 let nome = document.getElementById("name");
 let phone = document.getElementById("phone");
 let birth = document.getElementById("birthdate");
@@ -7,6 +8,9 @@ let city = document.getElementById("city");
 let college = document.getElementById("college");
 let radios1 = document.getElementsByName("knowing");
 let radios2 = document.getElementsByName("radio2");
+let paymentRadios = document.getElementsByName("payment");
+let loadingDiv = document.getElementById('loading');
+let body = {};
 
 form.addEventListener("submit", function(stop){
 	stop.preventDefault();
@@ -19,6 +23,7 @@ form.addEventListener("submit", function(stop){
 		error = 0;
 		document.getElementById("nametitle").innerText = '';
 	}
+
 	if(/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(phone.value) == false){
 		error += 1;
 		document.getElementById("phonetitle").innerText = phone.title
@@ -27,6 +32,7 @@ form.addEventListener("submit", function(stop){
 		error = 0;
 		document.getElementById("phonetitle").innerText = ""
 	}
+
 	if(/^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+).([a-z])?$/.test(email.value) == false){
 		error += 1
 		document.getElementById("emailtitle").innerText = email.title;
@@ -35,6 +41,7 @@ form.addEventListener("submit", function(stop){
 		error = 0;
 		document.getElementById("emailtitle").innerText = ""
 	}
+
 	if(/[^0-9][a-z][^0-9]/ig.test(college.value) == false){
 		error += 1;
 		document.getElementById("collegetitle").innerText = college.title;
@@ -43,26 +50,7 @@ form.addEventListener("submit", function(stop){
 		error = 0
 		document.getElementById("collegetitle").innerText = "";
 	}
-	/*if do campo da cidade/
-	if(){
-		error += 1;
-		document.getElementById("citytitle").innerText = city.title ;
-	}
-	else{
-		error = 0;
-		document.getElementById("citytitle").innerText = "";
-	}
-	*/
-	/* if do campo birthdate
-	if(){
-		error += 1
-		document.getElementById("birthtitle").innerText = birthdate.title 
-	}
-	else{
-		error = 0
-		document.getElementById("birthtitle").innerText = "";
-	}
-	*/
+
 	if (radios1[0].checked === false && radios1[1].checked === false && radios1[2].checked === false && radios1 [3].checked === false && radios1[4].checked === false && radios1[5].checked === false && radios1[6].checked === false){
 		error += 1
 		document.getElementById("radios1title").innerText = "Escolha uma Opção"; 
@@ -71,6 +59,7 @@ form.addEventListener("submit", function(stop){
 		error = 0;
 		document.getElementById("radios1title").innerText = "";
 	}
+
 	if (radios2[0].checked === false && radios2[1].checked === false && radios2[2].checked === false && radios2 [3].checked === false && radios2[4].checked === false && radios2[5].checked === false && radios2[6].checked === false){
 		error += 1
 		document.getElementById("radios2title").innerText = "Escolha um opção";
@@ -79,25 +68,61 @@ form.addEventListener("submit", function(stop){
 		error = 0;
 		document.getElementById("radios2title").innerText = "";
 	}
+
 	if (error === 0){
-		postDataForm();
+		savePersonalData()
+		callPaymetnForm()
 	}
 
 })
 
+formPayment.addEventListener("submit", function(stop) {
+	stop.preventDefault();
+	let error = 0;
+
+	if (!paymentRadios[0].checked && !paymentRadios[1].checked && !paymentRadios[2].checked){
+		error += 1
+		document.getElementById("paymentRadioTitle").innerText = "Escolha um opção";
+	}
+	else{
+		error = 0;
+		document.getElementById("paymentRadioTitle").innerText = "";
+	}
+
+	if (error === 0){
+		savePayment()
+		postDataForm()
+	}
+})
+
 async function postDataForm() {
 	const url = 'https://formengcomprapi.azurewebsites.net/v1/subs';
-	const body = prepareDataToBody()
 	console.log('body', body)
-
+	showSpinner();
 	const response = await fetch(url, {
 		method: "POST",
 		body: JSON.stringify(body),
 		headers: {"Content-type": "application/json; charset=UTF-8"}
 	})
-};
+	console.log('response', response);
+	hideSpinner();
+	console.log('terminou')
+	
+}
 
-function prepareDataToBody() {
+function savePayment() {
+	let paymentChecked;
+	console.log('paymentRadios', paymentRadios)
+	for (var i = 0; i < paymentRadios.length; i++) {
+		if (paymentRadios[i].checked) {
+			paymentChecked = paymentRadios[i]
+		}
+	}
+
+	body.paycheck = paymentChecked.value;
+}
+
+function savePersonalData() {
 	let KnowingChecked;
 	let degreeChecked;
 
@@ -112,7 +137,7 @@ function prepareDataToBody() {
 		}
 	}
 
-	return {
+	body = {
 		name: nome.value,
 		phone: phone.value,
 		birthdate: birth.value,
@@ -121,6 +146,18 @@ function prepareDataToBody() {
 		college: college.value,
 		degree: degreeChecked.value,
 		knowing: KnowingChecked.value,
-		paycheck: 'Pix mano!',
-	}
+	}	
+}
+
+function callPaymetnForm() {
+	form.classList.add("hide");
+	formPayment.classList.add("show");
+}
+
+function showSpinner() {
+  loadingDiv.style.visibility = 'visible';
+}
+
+function hideSpinner() {
+  loadingDiv.style.visibility = 'hidden';
 }
