577658119a12b1ef976629e07590fdc913a76b16
Italo <italosp00@gmail.com>
2021-08-30 21:48:13 -0300

    Add the json credentials to the env

--- src/api/utils/spreadsheet.js
+++ src/api/utils/spreadsheet.js
@@ -2,12 +2,14 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 const dotenv = require("dotenv");
 
 dotenv.config();
-const creds = require(process.env.GOOGLE_KEY);
 
 module.exports = {
   async addRow(row) {
     const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
-    await doc.useServiceAccountAuth(creds);
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
+    });
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0];
     await sheet.addRow(row);
